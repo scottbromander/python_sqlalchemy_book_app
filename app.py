f0017950bcbda9806d75e80ebe32d18b60b25491
Scott Bromander <scottbromander@gmail.com>
2021-04-04 21:47:48 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+# import models
+# main menu - add, search, analysis, exit, view
+# add books to the database
+# edit books
+# delete books
+# search function
+# data cleaning
+# loop that runs the program
+
+

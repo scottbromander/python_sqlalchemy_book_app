02d5f32be5bdfd174c7b7cb26864eae816459043
Scott Bromander <scottbromander@gmail.com>
2021-04-05 21:26:23 -0500

    main application loop and menu created

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from models import (Base, session, Book, engine)
 
-
-
 # import models
 # main menu - add, search, analysis, exit, view
+
+def menu():
+    while True:
+        print('''
+                \nPROGRAMMING BOOKS
+                \r1) Add Book
+                \r2) View all books
+                \r3) Search for book
+                \r4) Book Analysis
+                \r5) Exit
+                ''')
+        choice = input('What would you like to do? ')
+        if choice in ['1','2','3','4','5']:
+            return choice
+        else:
+            input('''
+                  \nPlease choose one of the options above.
+                  \rA number from 1-5
+                  \rPress any key to see the options again.''')
+
 # add books to the database
 # edit books
 # delete books
@@ -11,6 +29,26 @@ from models import (Base, session, Book, engine)
 # data cleaning
 # loop that runs the program
 
+def app():
+    app_running = True
+    while app_running:
+        choice = menu()
+        if choice == '1':
+            # add book
+            pass
+        elif choice == '2':
+            # view books
+            pass
+        elif choice == '3':
+            # search for book
+            pass
+        elif choice == '4':
+            # book analysis
+            pass
+        else:
+            print('GOODBYE')
+            app_running = False
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
+    app()

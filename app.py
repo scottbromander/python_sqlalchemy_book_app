a01758526d5a1aa25f13232113210d58890fb945
Scott Bromander <scottbromander@gmail.com>
2021-04-08 22:00:56 -0500

    adds ability to see all books

--- app.py
+++ app.py
@@ -113,7 +113,10 @@ def app():
 
         elif choice == '2':
             # view books
-            pass
+            for book in session.query(Book):
+                print(f'{book.id} | {book.title} | {book.author}')
+            input('\nPress enter to return to the main menu')
+
         elif choice == '3':
             # search for book
             pass
